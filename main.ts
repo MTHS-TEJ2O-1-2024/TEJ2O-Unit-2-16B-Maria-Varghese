be56fba1d27dcc0a5f620f3389aee71dae60223f
Maria-Varghese <maria.varghese@mths.ca>
2024-12-16 11:22:00 -0500

    Using bluetooth radio

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria Varghese
+ * Created on: Nov 2024
+ * This program uses bluetooth radio 
 */
 
-basic.showString('Hello, World!')
+radio.setGroup(1)
+basic.showIcon(IconNames.Happy)
+
+//variable
+let distanceToObject: number = 0
+
+//useing bluetooth radio 
+input.onButtonPressed(Button.A, function() {
+   distanceToObject = sonar.ping(
+        DigitalPin.P1, 
+        DigitalPin.P2, 
+        PingUnit.Centimeters)
+    basic.showNumber(distanceToObject)
+    basic.showString(' cm')
+        radio.sendString('Too close')
+    basic.showIcon(IconNames.Square)
+})
+
+radio.onReceivedString(function (receivedString) {
+
+    if (distanceToObject < 10) {
+        basic.showString(receivedString)
+    } 
+    basic.showIcon(IconNames.Happy)
+})
+
+//cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
